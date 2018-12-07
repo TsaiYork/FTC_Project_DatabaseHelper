4f9b6afca49f1480fd6cc0b9a203ee212a46cae0
York <adidas1101@hotmail.com>
2018-12-07 16:50:33 +0800

    finished all function

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
 
@@ -23,15 +25,55 @@ namespace Calculate
         public string Label { get; set; }
     }
 
+    public class data_all
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "Position")]
+        public int Position { get; set; }
+
+        [JsonProperty(PropertyName = "BeaconMinor")]
+        public int BeaconMinor { get; set; }
+
+        [JsonProperty(PropertyName = "RSS")]
+        public double RSS { get; set; }
+    }
+
+    public partial class fuzzy_membership_created
+    {
+
+        [JsonProperty(PropertyName = "BeaconMinor")]
+        public int BeaconMinor { get; set; }
+
+        [JsonProperty(PropertyName = "Label")]
+        public string Label { get; set; }
+
+        [JsonProperty(PropertyName = "Samples")]
+        public int Samples { get; set; }
+
+        [JsonProperty(PropertyName = "Max")]
+        public int Max { get; set; }
+
+        [JsonProperty(PropertyName = "Min")]
+        public int Min { get; set; }
+
+        [JsonProperty(PropertyName = "Mean")]
+        public double Mean { get; set; }
+
+        [JsonProperty(PropertyName = "SD")]
+        public double SD { get; set; }
+    }
+
     class Program
     {
         //設定AP、RP之數量與編號
         static List<int> APs = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         //暫存資料
-        static List<int> allData = new List<int>();
+        static List<double> allData = new List<double>();
         static double mean, entropy, variance, sd;
-        static int Max, Min;
+        static double Max, Min;
 
         //Database 之連接字串
         //static string connStr = "server=140.122.105.178;user=user;database=ftc_project;port=3306;password=0000;SslMode=none;";
@@ -43,163 +85,133 @@ namespace Calculate
             List<int> N = new List<int> { };
             List<int> M = new List<int> { };
             List<int> F = new List<int> { };
-            RPs.Add(N);
-            RPs.Add(M);
-            RPs.Add(F);
+            RPs.Add(N);     //RPs[0] is Near Set
+            RPs.Add(M);     //RPs[1] is Medium Set
+            RPs.Add(F);     //RPs[2] is Far Set
 
-            List<fuzzy_label> res = new List<fuzzy_label>();
-            res = Get_fuzzy_label_N(1);
+            List<data_all> DataRes = new List<data_all>();
+            List<fuzzy_label> LabelRes = new List<fuzzy_label>();
+            fuzzy_membership_created uploadData = new fuzzy_membership_created();
 
-            foreach (var data in res)
+            foreach (int AP in APs)
             {
-                Console.WriteLine(data.Position);
+                //N
+                LabelRes.Clear();
+                LabelRes = Get_fuzzy_label_N(AP);
+                foreach (var data in LabelRes)
+                {
+                    RPs[0].Add(data.Position);
+                }
+                //RPs[0].ForEach(Console.Write);
+
+                foreach (int Position in RPs[0])
+                {
+                    DataRes.Clear();
+                    DataRes = Get_data_all_PosMinor(Position, AP);
+                    foreach (var data in DataRes)
+                    {
+                        if (data.RSS <= 0)
+                            allData.Add(data.RSS);
+                    }
+                }
+                updateValue();
+                Console.WriteLine("AP:" + AP.ToString() + "   N");
+                Console.WriteLine("  S: " + allData.Count + "  S.D: " + sd + "  Mean: " + mean);
+
+                uploadData.BeaconMinor = AP;
+                uploadData.Label = "N";
+                uploadData.Samples = allData.Count;
+                uploadData.Max = (int)Max;
+                uploadData.Min = (int)Min;
+                uploadData.Mean = mean;
+                uploadData.SD = sd;
+
+                UploadMembership(uploadData);
+              
+                dataClear();
+                RPs[0].Clear();
+
+                Console.WriteLine();
+
+                //M
+                LabelRes.Clear();
+                LabelRes = Get_fuzzy_label_M(AP);
+                foreach (var data in LabelRes)
+                {
+                    RPs[1].Add(data.Position);
+                }
+                //RPs[1].ForEach(Console.Write);
+
+                foreach (int Position in RPs[1])
+                {
+                    DataRes.Clear();
+                    DataRes = Get_data_all_PosMinor(Position, AP);
+                    foreach (var data in DataRes)
+                    {
+                        if (data.RSS <= 0)
+                            allData.Add(data.RSS);
+                    }
+                }
+                updateValue();
+                Console.WriteLine("AP:" + AP.ToString() + "   M");
+                Console.WriteLine("  S: " + allData.Count + "  S.D: " + sd + "  Mean: " + mean);
+
+                uploadData.BeaconMinor = AP;
+                uploadData.Label = "M";
+                uploadData.Samples = allData.Count;
+                uploadData.Max = (int)Max;
+                uploadData.Min = (int)Min;
+                uploadData.Mean = mean;
+                uploadData.SD = sd;
+
+                UploadMembership(uploadData);
+
+                dataClear();
+                RPs[1].Clear();
+
+                Console.WriteLine();
+
+                //F
+                LabelRes.Clear();
+                LabelRes = Get_fuzzy_label_F(AP);
+                foreach (var data in LabelRes)
+                {
+                    RPs[2].Add(data.Position);
+                }
+                RPs[2].ForEach(Console.Write);
+
+                foreach (int Position in RPs[2])
+                {
+                    DataRes.Clear();
+                    DataRes = Get_data_all_PosMinor(Position, AP);
+                    foreach (var data in DataRes)
+                    {
+                        if(data.RSS<=0)
+                            allData.Add(data.RSS);
+                    }
+                }
+                updateValue();
+                Console.WriteLine("AP:" + AP.ToString() + "   F");
+                Console.WriteLine("  S: " + allData.Count + "  S.D: " + sd + "  Mean: " + mean);
+
+                uploadData.BeaconMinor = AP;
+                uploadData.Label = "F";
+                uploadData.Samples = allData.Count;
+                uploadData.Max = (int)Max;
+                uploadData.Min = (int)Min;
+                uploadData.Mean = mean;
+                uploadData.SD = sd;
+
+                UploadMembership(uploadData);
+
+                dataClear();
+                RPs[2].Clear();
+
+                Console.WriteLine();
             }
-            Console.ReadKey();
-            //MySqlConnection conn = new MySqlConnection(connStr);
-            //MySqlCommand command = conn.CreateCommand();
 
-            //foreach (int AP in APs)
-            //{
-            //    Console.WriteLine();
-            //    Console.WriteLine("AP=" + AP.ToString());
-            //    try
-            //    {
-            //        Console.WriteLine("Connecting to MySQL...");
-            //        Console.WriteLine();
-            //        conn.Open();
-
-            //        //由資料庫選取 Fuzzy Label 為 'N' 的資料
-            //        string sqlQuery = "SELECT * FROM `ftc_office_fuzzy_label` WHERE `AP` = " + AP + " AND `Label` LIKE 'N'";
-            //        MySqlCommand cmd = new MySqlCommand(sqlQuery, conn);
-            //        MySqlDataReader rdr = cmd.ExecuteReader();
-            //        if (rdr.HasRows)
-            //        {
-            //            while (rdr.Read())
-            //            {
-            //                //Console.WriteLine(rdr[1]+"    "+ rdr[2] + "    " + rdr[3] + "    " );
-            //                RPs[0].Add(Convert.ToInt16(rdr[2]));
-            //            }
-            //        }
-            //        rdr.Close();
-            //        RPs[0].ForEach(Console.Write);
-            //        foreach (int i in RPs[0])
-            //        {
-            //            sqlQuery = "select * from `ftc_office_data` WHERE `Position` = " + i + " AND `BeaconMinor` = " + AP;
-            //            cmd = new MySqlCommand(sqlQuery, conn);
-            //            rdr = cmd.ExecuteReader();
-            //            if (rdr.HasRows)
-            //            {
-            //                while (rdr.Read())
-            //                {
-            //                    if (Convert.ToInt16(rdr[6]) != 999)
-            //                    {
-            //                        allData.Add(Convert.ToInt16(rdr[6]));
-            //                    }
-            //                }
-            //            }
-            //            rdr.Close();
-            //        }
-            //        updateValue();
-            //        Console.WriteLine("  S: " + allData.Count + "  S.D: " + sd + "  Mean: " + mean);
-            //        uploadData(command, conn, AP, "N");
-            //        dataClear();
-            //        RPs[0].Clear();
-
-            //        Console.WriteLine();
-
-            //        //M
-            //        sqlQuery = "SELECT * FROM `ftc_office_fuzzy_label` WHERE `AP` = " + AP + " AND `Label` LIKE 'M'";
-            //        cmd = new MySqlCommand(sqlQuery, conn);
-            //        rdr = cmd.ExecuteReader();
-            //        if (rdr.HasRows)
-            //        {
-            //            while (rdr.Read())
-            //            {
-            //                //Console.WriteLine(rdr[1] + "    " + rdr[2] + "    " + rdr[3] + "    ");
-            //                RPs[1].Add(Convert.ToInt16(rdr[2]));
-            //            }
-            //        }
-            //        rdr.Close();
-            //        RPs[1].ForEach(Console.Write);
-            //        foreach (int i in RPs[1])
-            //        {
-            //            sqlQuery = "select * from `ftc_office_data` WHERE `Position` = " + i + " AND `BeaconMinor` = " + AP;
-            //            cmd = new MySqlCommand(sqlQuery, conn);
-            //            rdr = cmd.ExecuteReader();
-            //            if (rdr.HasRows)
-            //            {
-            //                while (rdr.Read())
-            //                {
-            //                    if (Convert.ToInt16(rdr[6]) != 999)
-            //                    {
-            //                        allData.Add(Convert.ToInt16(rdr[6]));
-            //                    }
-            //                }
-            //            }
-            //            rdr.Close();
-            //        }
-            //        updateValue();
-            //        Console.WriteLine("  S: " + allData.Count + "  S.D: " + sd + "  Mean: " + mean);
-            //        uploadData(command, conn, AP, "M");
-            //        dataClear();
-            //        RPs[1].Clear();
-
-
-            //        Console.WriteLine();
-
-            //        //F
-            //        sqlQuery = "SELECT * FROM `ftc_office_fuzzy_label` WHERE `AP` = " + AP + " AND `Label` LIKE 'F'";
-            //        cmd = new MySqlCommand(sqlQuery, conn);
-            //        rdr = cmd.ExecuteReader();
-            //        if (rdr.HasRows)
-            //        {
-            //            while (rdr.Read())
-            //            {
-            //                //Console.WriteLine(rdr[1] + "    " + rdr[2] + "    " + rdr[3] + "    ");
-            //                RPs[2].Add(Convert.ToInt16(rdr[2]));
-            //            }
-            //        }
-            //        rdr.Close();
-            //        RPs[2].ForEach(Console.Write);
-            //        foreach (int i in RPs[2])
-            //        {
-            //            sqlQuery = "select * from `ftc_office_data` WHERE `Position` = " + i + " AND `BeaconMinor` = " + AP;
-            //            cmd = new MySqlCommand(sqlQuery, conn);
-            //            rdr = cmd.ExecuteReader();
-            //            if (rdr.HasRows)
-            //            {
-            //                while (rdr.Read())
-            //                {
-            //                    if (Convert.ToInt16(rdr[6]) != 999)
-            //                    {
-            //                        allData.Add(Convert.ToInt16(rdr[6]));
-            //                    }
-            //                }
-            //            }
-            //            rdr.Close();
-            //        }
-            //        updateValue();
-            //        Console.WriteLine("  S: " + allData.Count + "  S.D: " + sd + "  Mean: " + mean);
-            //        uploadData(command, conn, AP, "F");
-            //        dataClear();
-            //        RPs[2].Clear();
-
-            //        Console.WriteLine();
-
-            //        //___________________________________________________________________________
-
-
-
-            //        Console.WriteLine("Finished, Press Any Key to stop the program");
-            //        Console.ReadKey();
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        Console.WriteLine(ex.ToString());
-            //        Console.ReadKey();
-            //    }
-            //    conn.Close();
-            //}
+            Console.WriteLine("Finished, Press Any Key to stop the program");
+            Console.ReadKey();  
         }
 
         static void updateValue()
@@ -267,8 +279,8 @@ namespace Calculate
         static List<fuzzy_label> Get_fuzzy_label_N(int BeaconMinor)
         {
             List<fuzzy_label> fuzzy_label = new List<fuzzy_label>();
-            string aa = "http://" + BaseUrl + "api/fuzzy_label_N?BeaconMinor=" + BeaconMinor.ToString();
-            WebRequest URI = WebRequest.Create(aa);
+            string url = "http://" + BaseUrl + "api/fuzzy_label_N?BeaconMinor=" + BeaconMinor.ToString();
+            WebRequest URI = WebRequest.Create(url);
             URI.Method = "GET";
             using (HttpWebResponse response = (HttpWebResponse)URI.GetResponse())
             {
@@ -294,6 +306,126 @@ namespace Calculate
                 {
                     return null;
                 }
+            }
+        }
+
+        static List<fuzzy_label> Get_fuzzy_label_M(int BeaconMinor)
+        {
+            List<fuzzy_label> fuzzy_label = new List<fuzzy_label>();
+            string url = "http://" + BaseUrl + "api/fuzzy_label_M?BeaconMinor=" + BeaconMinor.ToString();
+            WebRequest URI = WebRequest.Create(url);
+            URI.Method = "GET";
+            using (HttpWebResponse response = (HttpWebResponse)URI.GetResponse())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    // Get the stream containing content returned by the server.  
+                    Stream dataStream = response.GetResponseStream();
+
+                    // Open the stream using a StreamReader for easy access.  
+                    StreamReader reader = new StreamReader(dataStream);
+
+                    // Read the content.  
+                    string responseFromServer = reader.ReadToEnd();
+
+                    fuzzy_label = Newtonsoft.Json.JsonConvert.DeserializeObject<List<fuzzy_label>>(responseFromServer);
+
+                    // Clean up the streams and the response.  
+                    reader.Close();
+                    response.Close();
+                    return fuzzy_label;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        static List<fuzzy_label> Get_fuzzy_label_F(int BeaconMinor)
+        {
+            List<fuzzy_label> fuzzy_label = new List<fuzzy_label>();
+            string url = "http://" + BaseUrl + "api/fuzzy_label_F?BeaconMinor=" + BeaconMinor.ToString();
+            WebRequest URI = WebRequest.Create(url);
+            URI.Method = "GET";
+            using (HttpWebResponse response = (HttpWebResponse)URI.GetResponse())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    // Get the stream containing content returned by the server.  
+                    Stream dataStream = response.GetResponseStream();
+
+                    // Open the stream using a StreamReader for easy access.  
+                    StreamReader reader = new StreamReader(dataStream);
+
+                    // Read the content.  
+                    string responseFromServer = reader.ReadToEnd();
+
+                    fuzzy_label = Newtonsoft.Json.JsonConvert.DeserializeObject<List<fuzzy_label>>(responseFromServer);
+
+                    // Clean up the streams and the response.  
+                    reader.Close();
+                    response.Close();
+                    return fuzzy_label;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        static List<data_all> Get_data_all_PosMinor(int Position,int BeaconMinor)
+        {
+            List<data_all> fuzzy_label = new List<data_all>();
+            string url = "http://" + BaseUrl + "api/data_all?Position="+Position.ToString()+"&&BeaconMinor=" + BeaconMinor.ToString();
+            WebRequest URI = WebRequest.Create(url);
+            URI.Method = "GET";
+            using (HttpWebResponse response = (HttpWebResponse)URI.GetResponse())
+            {
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    // Get the stream containing content returned by the server.  
+                    Stream dataStream = response.GetResponseStream();
+
+                    // Open the stream using a StreamReader for easy access.  
+                    StreamReader reader = new StreamReader(dataStream);
+
+                    // Read the content.  
+                    string responseFromServer = reader.ReadToEnd();
+
+                    fuzzy_label = Newtonsoft.Json.JsonConvert.DeserializeObject<List<data_all>>(responseFromServer);
+
+                    // Clean up the streams and the response.  
+                    reader.Close();
+                    response.Close();
+                    return fuzzy_label;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        static string UploadMembership(fuzzy_membership_created newData)
+        {
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                    string json = JsonConvert.SerializeObject(newData);
+                    HttpContent contentPost = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage res = client.PutAsync("http://" + BaseUrl + "api/fuzzy_membership", contentPost).Result;
+
+                    return res.Content.ReadAsStringAsync().Result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
             }
         }
 
